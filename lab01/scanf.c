2a8169d9890f0656d4fe871391ad6aa9e5b847f6
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-08-06 19:49:33 -0300

    Add files via upload

---
+++ lab01/scanf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    //Lista 1 Ex. 1
+
+    int x;
+
+    printf("Digite um número: ");
+    scanf("%d", &x);
+    printf("O número digitado foi: %d\n", x);
+
+    return 0;
+}
