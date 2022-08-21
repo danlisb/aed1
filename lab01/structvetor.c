8061ac9b24f578615c0cb5a5b7beabff89f5bab4
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-08-21 15:31:57 -0300

    Add files via upload

--- lab01/structvetor.c
+++ lab01/structvetor.c
@@ -1,25 +1,30 @@
+/*Daniel Lisboa
+Lista 6 Exercício 5
+Considerando a estrutura
+struct Vetor{
+    float x;
+    float y;
+    float z;
+};
+para representar um vetor no R3, implemente um programa que calcule a soma de dois vetores.*/
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    //Lista 6 Ex. 5
-
+int main() {
     struct Vetor {
         float x;
         float y;
         float z;
     };
 
-    Vetor.x = 10;
-    Vetor.y = 20;
-    Vetor.z = 30;
-    
-    void somaVetores() {
-        printf("A soma do vetor x e y é: %.2f.", Vetor.x + Vetor.y);
-        printf("A soma do vetor y e z é: %.2f.", Vetor.y + Vetor.z);
-        printf("A soma do vetor x e z é: %.2f.", Vetor.x + Vetor.z);
-    };
+    struct Vetor vetores;
+    vetores.x = 10;
+    vetores.y = 20;
+    vetores.z = 30;
 
-    somaVetores();
+    printf("A soma do vetor x e y é: %.2f.\n", vetores.x + vetores.y);
+    printf("A soma do vetor y e z é: %.2f.\n", vetores.y + vetores.z);
+    printf("A soma do vetor x e z é: %.2f.\n", vetores.x + vetores.z);
 
     return 0;
 }
