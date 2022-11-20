a66f5e12c01948214fb4e055a2bb66b1aff97b87
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-11-19 21:24:47 -0300

    Update atv4final.c

--- sem14/atv4final.c
+++ sem14/atv4final.c
@@ -11,21 +11,21 @@ int main()
                                                                //(considerando que falta um elemento no vetor nums[])
     memset(vetAux, 0, sizeof(int) * (numsSize + 1)); // coloca 0 no vetor auxiliar usando o memset
 
-    for (int i = 0; i < numsSize + 1; i++) // 
+    for (int i = 0; i < numsSize + 1; i++) 
     {
-        printf("[%d]", vetAux[i]);
+        printf("[%d]", vetAux[i]); // printa o vetor auxiliar preenchido com 0
     }
     printf("\n");
-
+ 
     for (int i = 0; i < numsSize; i++)
     {
-        printf("[%d]", nums[i]);
+        printf("[%d]", nums[i]); // printa os elementos do vetor nums[]   
     }
 
     for (int j = 0; j < numsSize; j++)
     {
-        tmp = nums[j];
-        vetAux[tmp] = 1;
+        tmp = nums[j]; // usamos uma variavel temporaria para armazenar os valores do vetor nums[]
+        vetAux[tmp] = 1; // se ele acha o numero correspondente no vetor temporario eh colocado 1 no vetor
     }
 
     printf("\n");
@@ -37,14 +37,14 @@ int main()
 
     for (int i = 0; i < numsSize + 1; i++)
     {
-        if (vetAux[i] == 0)
+        if (vetAux[i] == 0) // o elemento que nao tiver 1 tem 0, eh o que falta
         {
-            printf("[%d]", i);
+            printf("[%d]", i); // printamos o elemento com 0
             break;
         }
     }
     printf("\n");
 
-    free(vetAux);
+    free(vetAux); // limpa o lixo colocado
     return 0;
 }
